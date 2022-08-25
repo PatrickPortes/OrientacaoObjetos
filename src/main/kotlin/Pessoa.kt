768a1b366a1738d162f17f1dfd906a8dac35d124
Patrick Portes <89034086+PatrickPortes@users.noreply.github.com>
2022-08-25 18:04:05 -0300

    Initial commit

---
+++ src/main/kotlin/Pessoa.kt
@@ -0,0 +1,16 @@
+class Pessoa {
+
+    var nome: String = "Patrick"
+
+    var cpf: String = "000.000.000-01"
+
+}
+
+fun main(){
+
+    val patrick = Pessoa()
+
+    println(patrick.nome)
+    println(patrick.cpf)
+
+}
