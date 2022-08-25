d31704d5c201849c25f01e72bf80e79091f6abef
Patrick Portes <89034086+PatrickPortes@users.noreply.github.com>
2022-08-25 19:17:06 -0300

    Membros das Classes
    Update com pratica de membros de classes, inner and outer class, e inicio de modificadores de acesso

--- src/main/kotlin/Pessoa.kt
+++ src/main/kotlin/Pessoa.kt
@@ -1,8 +1,15 @@
 class Pessoa {
 
     var nome: String = "Patrick"
-
     var cpf: String = "000.000.000-01"
+        private set
+
+    inner class Endereco{
+
+        var rua: String = "Rua Japao"
+    }
+
+
 
 }
 
@@ -12,5 +19,7 @@ fun main(){
 
     println(patrick.nome)
     println(patrick.cpf)
+    println(patrick.Endereco().rua)
+
 
 }
